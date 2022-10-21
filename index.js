44c55b119a7567a74b4ee7f08f6b0eb60113fcda
G.Rizogiannis <rizogiannhsgeo@hotmail.com>
2022-10-21 20:47:04 +0300

    Changes for Heroku

--- index.js
+++ index.js
@@ -116,6 +116,6 @@ app.post('/user/update',(req,res)=>{
 });
 });
 
-app.listen(3000,()=>{
-    console.log('Listening on port 3000');
+app.listen(process.env.PORT || 5000,()=>{
+    console.log('Listening on port');
 });
